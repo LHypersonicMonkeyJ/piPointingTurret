c33bef7cfb444268e55b87ec5c6e9f52d2cb9364
longji <longji.yan@outlook.com>
2024-03-06 00:03:10 -0600

    add current time timer and initialize target buttons

--- pointee_test.py
+++ pointee_test.py
@@ -4,7 +4,7 @@ import time
 import can
 from datetime import datetime
 from PyQt5.QtCore import Qt, QTimer, QDateTime
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QLabel
 from pointee import Ui_Pointee
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
@@ -16,6 +16,7 @@ class PointeeApp(QMainWindow):
         self.initHardware()
         self.initUI()
         self.initTimer()
+        self.initButtons()
 
     def initHardware(self):
         self.pointing = pointing()
@@ -32,11 +33,16 @@ class PointeeApp(QMainWindow):
     def initTimer(self):
         """ timer init fastest to slowest """
         # timer for updating current time (1 second)
+        self.timer_current_time = QTimer(self)
+        self.timer_current_time.timeout.connect(self.update_current_time)
+        self.timer_current_time.start(1000)
 
-        # timer for upating target pointing (1 second)
+        # default timer for upating target pointing (1 second)
+        # this timer interval can be changed depends on selected pointing target
 
         # timer for updating current room weather (20 second)
         self.timer_room_weather = QTimer(self)
+        #self.timer_room_weather.setTimerType(Qt.PreciseTimer)
         self.timer_room_weather.timeout.connect(self.update_current_room_weather)
         self.timer_room_weather.start(20000)
 
@@ -46,7 +52,13 @@ class PointeeApp(QMainWindow):
 
         # timer for updating date (midnight)
 
-        
+    def initButtons(self):
+        """ Initialize buttons """
+        # get the available pointing targets from the pointing class
+        # if the button is clicked, change the button color to green
+        for target in self.pointing.available_targets:
+            targetButtonName = "target_" + target
+            self.ui.__getattribute__(targetButtonName).clicked.connect(lambda _, target=target: self.target_button_on_click(target))
 
     #===========================================================================
     # define update functions (fastest to slowest)
@@ -61,7 +73,8 @@ class PointeeApp(QMainWindow):
         self.ui.label_roomTemp.setText("{:.1f} °F".format(current_room_temp))
 
     def update_current_time(self):
-        pass
+        current_time = QDateTime.currentDateTime()
+        self.ui.label_currentTime.setText(current_time.toString("hh:mm:ss"))
 
     def update_outdoor_current_weather(self):
         pass
@@ -71,6 +84,16 @@ class PointeeApp(QMainWindow):
 
     def update_date(self):
         pass
+
+    def target_button_on_click(self, target):
+        """ Update the pointing target
+        1. Change the target button color to green
+        2. Request target ephemeris
+        3. Calculate the target pointing loop delta time
+        4. Update the target pointing loop Qtimer
+        5. Call function to update the target pointing 
+        """
+        print("target button clicked: {}".format(target))
 
 def main():
     app = QApplication(sys.argv)
